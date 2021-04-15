10766d4e4f38ca44cb360e6420ea04c27d32b9c4
Marcos Francisco Picosque <mpicosque@gmail.com>
2021-04-15 12:56:48 -0300

    Custom Constraintvalidator

--- backend/src/main/java/com/mpicosque/dscatalog/dto/UserInsertDTO.java
+++ backend/src/main/java/com/mpicosque/dscatalog/dto/UserInsertDTO.java
@@ -2,6 +2,9 @@ package com.mpicosque.dscatalog.dto;
 
 import java.io.Serializable;
 
+import com.mpicosque.dscatalog.services.validation.UserInsertValid;
+
+@UserInsertValid
 public class UserInsertDTO extends UserDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
